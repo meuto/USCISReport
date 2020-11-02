90cf1a24fafa770ac16bdd1ede1787553438c074
meuto <30797550+meuto@users.noreply.github.com>
2020-11-02 14:01:45 -0600

    Update and ready to send To Raspberry

--- USCIS.py
+++ USCIS.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import smtplib
 import os
 import datetime
+import pandas as pd
 headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'}
 situacion=''
 comentarios=''
@@ -25,22 +26,10 @@ fecha=fecha[3:]
 data=datetime.datetime.strptime(fecha,'%B %d %Y')
 deltaDays=abs((data-datetime.datetime.now()).days)
 print(f'Days since las update: {deltaDays}')
-path='C:\\users\\tomeu\\OneDrive\\Desktop\\USCIS\\'
+path='C:\\Users\\tomeu\\Source\\Repos\\USCISReport'
 tome=os.listdir(path)
-if not tome:
-    f=open(path+'situacion.txt',"w")
-    h=open(path+'comentarios.txt',"w")
-    f.write(soup.find("div", {"class": "rows text-center"}).select('h1')[0].text)
-    h.write(soup.find("div", {"class": "rows text-center"}).select('p')[0].text)
-    f.close()
-    h.close()
-else:
-    f=open(path+'situacion.txt',"r")
-    h=open(path+'comentarios.txt',"r")
-    situacion=f.read()
-    comentarios=h.read()
-    f.close()
-    h.close()
+situation=[soup.find("div", {"class": "rows text-center"}).select('h1')[0].text]
+coments=[soup.find("div", {"class": "rows text-center"}).select('p')[0].text]
 def funcemail(email,password,emailTo,subject,Body):
     smtp=smtplib.SMTP('smtp.gmail.com',587)
     smtp.ehlo()
@@ -50,20 +39,27 @@ def funcemail(email,password,emailTo,subject,Body):
     msg=f'Subject:{subject}\n\n{Body}'
     smtp.sendmail(email,emailTo,msg)
 
-if situacion!=soup.find("div", {"class": "rows text-center"}).select('h1')[0].text:
-    f=open(path+'situacion.txt',"w")
-    h=open(path+'comentarios.txt',"w")
-    f.write(soup.find("div", {"class": "rows text-center"}).select('h1')[0].text)
-    h.write(soup.find("div", {"class": "rows text-center"}).select('p')[0].text)
-    f.close()
-    h.close()
-    f=open(path+'situacion01.txt',"w")
-    h=open(path+'comentarios01.txt',"w")
-    f.write(situacion)
-    h.write(comentarios)
-    f.close()
-    h.close()
+if 'historic.csv' not in tome:
+    historic={'situation':situation,'coments':coments,'USCISCase':dataForm['appReceiptNum']}
+    historic=pd.DataFrame(historic)
+    historic.to_csv(path+'\\historic.csv',header=True,index=False,sep=';')
     asunto='Change of Status-'+soup.find("div", {"class": "rows text-center"}).select('h1')[0].text
-    cuerpo=soup.find("div", {"class": "rows text-center"}).select('p')[0].text
-
+    cuerpo=soup.find("div", {"class": "rows text-center"}).select('p')[0].text+f'\n \n Days Since last Update: {deltaDays}'
     funcemail(email,password,emailTo,asunto,cuerpo)
+
+else:
+    historic=pd.read_csv(path+'\\historic.csv',sep=';')
+    if len(historic)==1:
+        print('nothing')
+    else:
+        historicChange=[situation,coments,dataForm['appReceiptNum']]
+        if (historic.iloc[-1,:][0]==situation) and (historic.iloc[-1,:][1]==coments):
+            print('nothing')
+        else:
+            historic=pd.concat([historic,pd.DataFrame(historicChange)],ignore_index=True)
+            historic.to_csv(path+'\\historic.csv',header=True,index=False,sep=';')
+            asunto='Change of Status-'+soup.find("div", {"class": "rows text-center"}).select('h1')[0].text
+            cuerpo=soup.find("div", {"class": "rows text-center"}).select('p')[0].text+f'\n \n Days Since last Update: {deltaDays}'
+            funcemail(email,password,emailTo,asunto,cuerpo)
+
+
